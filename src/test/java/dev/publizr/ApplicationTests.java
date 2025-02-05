119d809434014a088d18e462eb582d6380466d55
Chigbogu Orji <brightorji60@gmail.com>
2025-02-05 21:44:37 +0100

    style: remove target folder from git

--- src/test/java/dev/publizr/ApplicationTests.java
+++ src/test/java/dev/publizr/ApplicationTests.java
@@ -1,13 +1,13 @@
-package dev.publisher;
+package dev.publizr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PublisherApplicationTests {
+class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
