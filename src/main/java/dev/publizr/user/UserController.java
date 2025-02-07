4598efcbfcb146e59e9814ee48ac33e8a99d2fe3
Chigbogu Orji <brightorji60@gmail.com>
2025-02-08 00:34:57 +0100

    refactor: refactor user signup

--- src/main/java/dev/publizr/user/UserController.java
+++ src/main/java/dev/publizr/user/UserController.java
@@ -1,35 +1,60 @@
 package dev.publizr.user;
 
+import dev.publizr.jwt.JWTService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
+	private final JWTService jwtService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+	public UserController(UserRepository userRepository, JWTService jwtService) {
+		this.userRepository = userRepository;
+		this.jwtService = jwtService;
+	}
 
-    @GetMapping("")
-    List<UserDTO> findAll() {
-        return userRepository.findAll();
-    }
+	@GetMapping("")
+	List<UserDTO> findAll() {
+		return userRepository.findAll();
+	}
 
-    @GetMapping("/{id}")
-    UserDTO findById(@PathVariable Integer id) {
-        Optional<UserDTO> user = userRepository.findById(id);
-        return user.orElse(null);
-    }
+	@GetMapping("/{id}")
+	UserDTO findById(@PathVariable Integer id) {
+		Optional<UserDTO> user = userRepository.findById(id);
+		return user.orElse(null);
+	}
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("")
-    void create(@Valid @RequestBody User user) {
-        userRepository.save(user);
-    }
+	@PostMapping("")
+	ResponseEntity<Map<String, String>> signUp(@Valid @RequestBody User user) {
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+		if (!pattern.matcher(user.email()).matches())
+			throw new RuntimeException(String.format("EMAIL '%S' IS INVALID", user.email().toUpperCase()));
+
+		Integer createdUserId = null;
+		try {
+			Integer emailCount = userRepository.findUserByEmailAddress(user.email());
+			if (emailCount > 0)
+				throw new RuntimeException(String.format("USER WITH EMAIL '%S' ALREADY EXIST", user.email()));
+
+			createdUserId = userRepository.createUser(user);
+		} catch (RuntimeException e) {
+			throw new RuntimeException(e);
+		}
+
+		User createdUser = userRepository.findByUserId(createdUserId);
+		String jwtToken = jwtService.generateJWTToken(createdUser);
+		Map<String, String> map = new HashMap<>();
+		map.put("token", jwtToken);
+		return new ResponseEntity<>(map, HttpStatus.CREATED);
+	}
 }
--- src/main/java/dev/publizr/user/UserRepository.java
+++ src/main/java/dev/publizr/user/UserRepository.java
@@ -1,35 +1,54 @@
 package dev.publizr.user;
 
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Repository
 public class UserRepository {
-    private final JdbcClient jdbcClient;
+	private final JdbcClient jdbcClient;
 
-    public UserRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
+	public UserRepository(JdbcClient jdbcClient) {
+		this.jdbcClient = jdbcClient;
+	}
 
-    public void save(User user) {
-        var created = jdbcClient.sql("INSERT INTO USERS (USERNAME, EMAIL, PASSWORD) VALUES (?, ?, ?)")
-                .params(List.of(user.username(), user.email(), user.password()))
-                .update();
-        Assert.state(created == 1, "failed to create user " + user.username());
-    }
+	public Integer createUser(User user) {
+		try {
+			KeyHolder keyHolder = new GeneratedKeyHolder();
+			var created = jdbcClient.sql("INSERT INTO USERS (USERNAME, EMAIL, PASSWORD) VALUES (?, ?, ?)")
+				.params(List.of(user.username(), user.email(), user.password()))
+				.update(keyHolder);
+			Assert.state(created == 1, "failed to create user " + user.username());
+			return (Integer) Objects.requireNonNull(keyHolder.getKeys()).get("USER_ID");
+		} catch (RuntimeException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public List<UserDTO> findAll() {
-        return jdbcClient.sql("SELECT * FROM USERS").query(UserDTO.class).list();
-    }
+	public List<UserDTO> findAll() {
+		return jdbcClient.sql("SELECT * FROM USERS").query(UserDTO.class).list();
+	}
 
-    public Optional<UserDTO> findById(Integer id) {
-        return jdbcClient.sql("SELECT * FROM USERS WHERE ID = :ID")
-                .param("ID", id)
-                .query(UserDTO.class)
-                .optional();
-    }
+	public Optional<UserDTO> findById(Integer id) {
+		return jdbcClient.sql("SELECT * FROM USERS WHERE ID = :ID")
+			.param("ID", id)
+			.query(UserDTO.class)
+			.optional();
+	}
+
+	public Integer findUserByEmailAddress(final String email) {
+		return jdbcClient.sql("SELECT * FROM USERS WHERE EMAIL = :EMAIL")
+			.param("EMAIL", email).query(User.class).list().size();
+	}
+
+	public User findByUserId(Integer id) {
+		return jdbcClient.sql("SELECT * FROM USERS WHERE USER_ID = :USER_ID")
+			.param("USER_ID", id).query(User.class).single();
+	}
 }
